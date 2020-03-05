ee1f4f90bc720a626e860ccb40ae5f2d3e56b2e2
frances96 <54961324+frances96@users.noreply.github.com>
2020-03-05 10:43:28 -0500

    Create TestPlayList.java

---
+++ TestPlayList.java
@@ -0,0 +1,44 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TestPlayList {
+  public static void main(String[] args) throws FileNotFoundException {
+	// TODO Auto-generated method stub
+	char ch  = 'y';
+	ArrayList<String> files = new ArrayList<String>();
+	ArrayList<MyQueue> allTheWeeks = new ArrayList<MyQueue>();
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Welcome to Playlist System!");
+	while (ch ==  'y' || ch == 'Y') {
+            System.out.println("Provide path to the file:");
+            files.add(sc.next());
+            System.out.println("Do you want to provide more files?");
+            ch = sc.next().charAt(0);
+  }
+	for (int i = 0; i<files.size(); i++) {
+            MyQueue data = new MyQueue(files.get(i));
+            allTheWeeks.add(i, data);
+	}
+  
+	PlayList playList = new PlayList();
+	MyQueue mergedQueue = allTheWeeks.get(0);
+	for(int i = 1; i<allTheWeeks.size()-1; i++) {
+            mergedQueue = playList.mergingFunction(allTheWeeks.get(i), mergedQueue);
+	}
+	while (!mergedQueue.isEmpty()) {
+            Node n = mergedQueue.dequeue();
+            playList.addSong(n.getItem());
+	}
+		
+	SongHistoryList history = new SongHistoryList();
+	Song temp = playList.listenToSong();
+		
+	while (temp != null) {
+            // Add the song to history if it is not already in the playlist
+            history.addSong(temp);
+            System.out.println("Listening to Song : " + temp.getTrack());
+            temp = playList.listenToSong();		
+	}
+ }	
+}
